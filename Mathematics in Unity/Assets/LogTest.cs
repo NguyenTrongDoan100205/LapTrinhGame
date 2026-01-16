d660f4f9d7bb4e34a2eb1dfc9637399f419fb705
NguyenTrongDoan100205 <nguyentrongdoanns@iCloud.com>
2026-01-16 22:36:12 +0700

    Nop bai tap: Da loai bo file 150MB va Library

---
+++ Mathematics in Unity/Assets/LogTest.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class LogTest : MonoBehaviour
+{
+    void Start()
+    {
+        // Thay [Tên Bạn] bằng tên thật của bạn để nộp bài
+        Debug.Log("HELLO! GAME CUA [NguyenTrong Doan] DANG CHAY TREN ANDROID!");
+    }
+}
+    
