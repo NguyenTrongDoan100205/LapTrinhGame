8c60b698d4b93292d74f97ecae6ced7e5a5a2a27
NguyenTrongDoan100205 <nguyentrongdoanns@iCloud.com>
2026-01-22 15:30:45 +0700

    Gộp lại các thư mục cũ và bài Demo Part 2

---
+++ Mathematics in Unity/Assets/WorldToScreen.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+
+public class WorldToScreen : MonoBehaviour
+{
+    void Update()
+    {
+        // Chuyển đổi tọa độ thế giới của Cube sang tọa độ màn hình Pixel
+        Vector3 screenPos = Camera.main.WorldToScreenPoint(transform.position);
+        // In kết quả ra bảng Console
+        Debug.Log("Screen Position: " + screenPos);
+    }
+}
