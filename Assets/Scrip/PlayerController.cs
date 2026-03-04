95d455187c7e967175adae303ec2013fe683f25a
NguyenTrongDoan100205 <nguyentrongdoanns@iCloud.com>
2026-03-04 11:15:46 +0700

    Hoan thanh Lap 1-8 va fix loi Input Unity 6

---
+++ Assets/Scrip/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f; // Tốc độ di chuyển
+
+    void Update()
+    {
+        // Lấy input từ bàn phím (Phím A/D hoặc Mũi tên Trái/Phải)
+        float moveInput = Input.GetAxis("Horizontal");
+
+        // Di chuyển tọa độ của nhân vật
+        transform.position += new Vector3(moveInput * speed * Time.deltaTime, 0, 0);
+
+        // Lật mặt nhân vật (Flip) khi đổi hướng
+        if (moveInput > 0) transform.localScale = new Vector3(1, 1, 1);
+        else if (moveInput < 0) transform.localScale = new Vector3(-1, 1, 1);
+    }
+}
