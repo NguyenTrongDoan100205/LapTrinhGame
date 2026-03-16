63f5b63ffead15566d14d21cfeb08b9e8e2d61c4
NguyenTrongDoan100205 <nguyentrongdoanns@iCloud.com>
2026-03-16 23:18:57 +0700

    Submit Lab 8 sạch sẽ

---
+++ Assets/Scrip/EnemySpawner.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    // Kéo Prefab Enemy vào ô này ở Inspector
+    public GameObject enemyPrefab;
+
+    void Update()
+    {
+        // Kiểm tra nếu người dùng nhấn phím Space
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Tạo ra một bản sao của Prefab tại vị trí của Spawner
+            Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+        }
+    }
+}
